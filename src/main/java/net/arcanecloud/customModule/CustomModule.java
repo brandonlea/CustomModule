7f0c6454f71aca224f5a41e4906b53edee28a96d
Brandon-Lea Price <brandonlea810@gmail.com>
2025-01-05 18:15:40 +0000

    Added to check for correct generator and rename.

--- src/main/java/net/arcanecloud/customModule/CustomModule.java
+++ src/main/java/net/arcanecloud/customModule/CustomModule.java
@@ -1,17 +1,41 @@
 package net.arcanecloud.customModule;
 
+import me.pikamug.quests.BukkitQuestsPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.AbstractMap;
+import java.util.Map;
 
 public final class CustomModule extends JavaPlugin {
 
+    private static final BukkitQuestsPlugin quests = (BukkitQuestsPlugin) Bukkit.getServer().getPluginManager().getPlugin("Quests");
+    private static final String moduleName = "ArcaneModule";
+    private static final Map.Entry<String, Short> moduleItem = new AbstractMap.SimpleEntry<>("DIAMOND", (short)0);
+
+    public static BukkitQuestsPlugin getQuests() {
+        return quests;
+    }
+
+    public static String getModuleName() {
+        return moduleName;
+    }
+
+    public static Map.Entry<String, Short> getModuleItem() {
+        return moduleItem;
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().severe(ChatColor.RED + "Move this jar to your " + File.separatorChar + "Quests" + File.separatorChar
+                + "modules folder!");
+        getServer().getPluginManager().disablePlugin(this);
+        setEnabled(false);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
--- src/main/java/net/arcanecloud/customModule/GenObjective.java
+++ src/main/java/net/arcanecloud/customModule/GenObjective.java
@@ -1,4 +1,56 @@
 package net.arcanecloud.customModule;
 
-public class GenObjective {
+import com.artillexstudios.axgens.AxGens;
+import com.artillexstudios.axgens.api.AxGensAPI;
+import com.artillexstudios.axgens.api.events.AxGensHookReloadEvent;
+import com.artillexstudios.axgens.api.events.GeneratorPostPlaceEvent;
+import com.artillexstudios.axgens.generators.Generator;
+import com.artillexstudios.axgens.user.AxGensUser;
+import me.pikamug.quests.module.BukkitCustomObjective;
+import me.pikamug.quests.quests.Quest;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+import java.util.Map;
+
+public class GenObjective extends BukkitCustomObjective
+{
+    public GenObjective() {
+        setName("Generator Objective");
+        setAuthor("ArcaneCloud");
+        setItem("DIAMOND_BLOCK", (short) 0);
+        setShowCount(true);
+        setCountPrompt("How many Generators would you like to place?");
+        addStringPrompt("Tier", "Please choose a tier", "None");
+        addStringPrompt("Name of generator", "Please select a name", "None");
+        setDisplay("%Name of generator%: %count%");
+    }
+
+    @Override
+    public String getModuleName() {
+        return CustomModule.getModuleName();
+    }
+
+    @Override
+    public Map.Entry<String, Short> getModuleItem() {
+        return CustomModule.getModuleItem();
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void PlaceGeneratorEvent(GeneratorPostPlaceEvent event) {
+        final Player player = event.getPlayer();
+        final Generator generator = event.getGenerator();
+        for(Quest quest : CustomModule.getQuests().getQuester(player.getUniqueId()).getCurrentQuests().keySet()) {
+            Map<String, Object> map = getDataForPlayer(player.getUniqueId(), this, quest);
+
+
+            if(map != null) {
+                int tier = Integer.parseInt((String) map.get("Tier"));
+                if(generator.getTier() == tier) {
+                    incrementObjective(player.getUniqueId(), this, quest, 1);
+                }
+            }
+        }
+    }
 }
